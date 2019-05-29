fd5e2c04243384589c257e458f1c50072921edf3
App Academy Student <student@appacademy.io>
2019-05-29 15:35:34 -0700

    Initial commit implement Course,User,Enrollment associations

---
+++ associations/app/models/course.rb
@@ -0,0 +1,56 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+
+class Course < ApplicationRecord
+
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Enrollment 
+  
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :user
+  #  has_many :enrolled_students,
+  # through: :student_id,
+  # source: :user
+  
+
+
+end
+
+
+
+
+
+
+
+
+
+
+# class Course < ApplicationRecord
+#   has_many :enrollments,
+#     primary_key: :id,
+#     foreign_key: :course_id,
+#     class_name: :Enrollment
+
+# end
+
+#class prereq < Course
+#  b
+#
+#end
+
+
+
+

6c4b35e919e81ad9f0ac30b56e83b7d4d44af934
App Academy Student <student@appacademy.io>
2019-05-29 17:22:29 -0700

    Initial commit URLshortner rails project

---
+++ URLShortener/db/migrate/20190529235058_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email,  null: false
+
+
+
+      t.timestamps
+
+    end
+
+    add_index :email, unique: true
+    
+  end
+end
